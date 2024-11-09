763c8f829d8ec29e863d92b4310fd292fe828c53
Antcating <yevhenii.fedorchenko@gmail.com>
2024-11-09 16:05:36 +0200

    Depth-weighted 1D Perlin noise generation implemented

--- Scripts/tile_map_layer.gd
+++ Scripts/tile_map_layer.gd
@@ -2,35 +2,51 @@ extends TileMapLayer
 var map_height = 480
 var map_width = 640
 
+func weight(h, h_min, h_max, smooth):
+	# Weighting function for 1D depth based generation
+	# See graph using Desmos: \left(\frac{-\left(\left(x-a\right)-\left(b-a\right)\right)^{c}+\left(b-a\right)^{c}}{\left(b-a\right)^{c}}\right)
+	return (- ((h - h_min) - ((h_max - h_min)/2)) ** smooth + ((h_max - h_min)/2) ** smooth) / (((h_max - h_min)/2) ** smooth)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var texture = NoiseTexture2D.new()
-	texture.width = map_width
-	texture.height = map_height
+	#var texture = NoiseTexture2D.new()
+	#texture.width = map_width
+	#texture.height = map_height
 	var noise = FastNoiseLite.new()
 	noise.seed = 12345
-	noise.noise_type = 3
-	noise.frequency = 0.07
+	noise.noise_type = 0
+	noise.fractal_octaves = 3
+	noise.frequency = 0.03
 	
-	texture.noise = noise
-	await texture.changed
-	
-	$Sprite2D.texture = texture
-	
-	for i in range(72):
-		for j in range(3):
-			set_cell(Vector2i(i, j), 0, Vector2i(15, 7), 0)
+	var map: Array[Array]
+	for i in range(map_width):
+		map.append([])
+		for j in range(map_width):
+			map[i].append(0)
 	
 	for i in range(map_width):
-		for j in range(3, map_height):
-			if noise.get_noise_2d(i, j) > 0.33:
-				set_cell(Vector2i(i, j), 0, Vector2i(18, 7), 0)
-			else:
-				
-				set_cell(Vector2i(i, j), 0, Vector2i(15, 7), 0)
+		for j in range(3, map_height/1.5):
+			if noise.get_noise_2d(i, j) * weight(j, -10.0, map_height/1.5, 2) > 0.33:
+				map[i][j] = 1
+	# Cleaning function
+	#for i in range(len(map)):
+		#for j in range(len(map[0])):
+			#if i > 0 and i < len(map)-1 and j > 0 and j < len(map[0])-1:
+				#if map[i][j] != 0:
+					#if map[i-1][j] == 0 and map[i+1][j] == 0 and map[i][j-1] == 0 and map[i][j+1] == 0:
+						#map[i][j] = 0
 	
-	pass # Replace with function body.
-
+	# Main drawing cycle 
+	for i in range(map_width):
+		for j in range(map_height):
+			if map[i][j] == 0:
+				# Add variation to the ground
+				if randf() > 0.5:
+					set_cell(Vector2i(i, j), 0, Vector2i(9, 3), 0)
+				else:
+					set_cell(Vector2i(i, j), 0, Vector2i(1, 3), 0)
+			elif map[i][j] == 1:
+				set_cell(Vector2i(i, j), 0, Vector2i(25, 3), 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):

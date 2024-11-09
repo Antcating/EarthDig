2f1ad67c78cd38dd69761729bfedbd82e24e2ce2
Antcating <yevhenii.fedorchenko@gmail.com>
2024-11-09 19:50:31 +0200

    Camera clean-up

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -7,20 +7,6 @@ func _ready():
 
 const speed = 1000
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#if Input.is_key_pressed(KEY_W):
-		#position.y -= 5
-		#
-	#elif Input.is_key_pressed(KEY_S):
-		#rigid_body_2d.linear_velocity.x += 1
-	#
-	#elif Input.is_key_pressed(KEY_A):
-		#position.x -= 5
-	#
-	#elif Input.is_key_pressed(KEY_D):
-		#position.x += 5
-		
 func _physics_process(delta):
 	var vel = speed * delta
 	if Input.is_key_pressed(KEY_W):
@@ -41,16 +27,16 @@ func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			if zoom.x < 1:
-				zoom.x *= 2
-				zoom.y *= 2
+				zoom.x *= 1.5
+				zoom.y *= 1.5
 			else:
 				zoom.x += 1
 				zoom.y += 1
 		
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			if zoom.x <= 1:
-				zoom.x /= 2
-				zoom.y /= 2
+				zoom.x /= 1.5
+				zoom.y /= 1.5
 			else:
 				zoom.x -= 1
 				zoom.y -= 1

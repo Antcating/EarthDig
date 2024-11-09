bb82a1f865704d5a3826851d0e012a82236e4337
Antcating <yevhenii.fedorchenko@gmail.com>
2024-11-09 17:57:16 +0200

    Debug FPS counter added

---
+++ Scripts/fps.gd
@@ -0,0 +1,15 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_visibility_layer(0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("show_debug"):
+		print(get_visibility_layer())
+		set_visibility_layer(!get_visibility_layer())
+	if get_visibility_layer():
+		set_text("FPS %d" % Engine.get_frames_per_second())

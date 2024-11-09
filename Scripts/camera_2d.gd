872bb6cfc66d2ac034cfdd3c8f423510486ccd88
Antcating <yevhenii.fedorchenko@gmail.com>
2024-11-09 16:04:52 +0200

    Zoom functionality added

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -1,7 +1,5 @@
 extends Camera2D
 
-@onready var rigid_body_2d = $RigidBody2D
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -28,13 +26,32 @@ func _physics_process(delta):
 	if Input.is_key_pressed(KEY_W):
 		position.y -= vel
 		
-	elif Input.is_key_pressed(KEY_S):
+	if Input.is_key_pressed(KEY_S):
 		position.y += vel
 	
-	elif Input.is_key_pressed(KEY_A):
+	if Input.is_key_pressed(KEY_A):
 		position.x -= vel
 	
-	elif Input.is_key_pressed(KEY_D):
+	if Input.is_key_pressed(KEY_D):
 		position.x += vel
+		
+		
 	
-	
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			if zoom.x < 1:
+				zoom.x *= 2
+				zoom.y *= 2
+			else:
+				zoom.x += 1
+				zoom.y += 1
+		
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			if zoom.x <= 1:
+				zoom.x /= 2
+				zoom.y /= 2
+			else:
+				zoom.x -= 1
+				zoom.y -= 1
+		

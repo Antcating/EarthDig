1befcd31ec43056159d300bbd75b4a1ba1d37d9e
Antcating <yevhenii.fedorchenko@gmail.com>
2024-11-09 19:51:20 +0200

    Exponential decay with leakage

--- Scripts/tile_map_layer.gd
+++ Scripts/tile_map_layer.gd
@@ -4,9 +4,14 @@ var map_width = 640
 
 func weight(h, h_min, h_max, smooth):
 	# Weighting function for 1D depth based generation
-	# See graph using Desmos: \left(\frac{-\left(\left(x-a\right)-\left(b-a\right)\right)^{c}+\left(b-a\right)^{c}}{\left(b-a\right)^{c}}\right)
-	return (- ((h - h_min) - ((h_max - h_min)/2)) ** smooth + ((h_max - h_min)/2) ** smooth) / (((h_max - h_min)/2) ** smooth)
 
+	# Strictly within boundaries 
+	# See graph using Desmos: \left(\frac{-\left(\left(x-a\right)-\left(b-a\right)\right)^{c}+\left(b-a\right)^{c}}{\left(b-a\right)^{c}}\right)
+	#return (- ((h - h_min) - ((h_max - h_min)/2)) ** smooth + ((h_max - h_min)/2) ** smooth) / (((h_max - h_min)/2) ** smooth)
+	
+	# With leakage 
+	# See graph e^{-\left(\frac{\left(x-a\right)-\frac{\left(b-a\right)}{2}}{c}\right)^{2}}
+	return exp(-((((h - h_min) - (h_max - h_min)/2)/smooth) ** 2))
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#var texture = NoiseTexture2D.new()
@@ -14,9 +19,9 @@ func _ready():
 	#texture.height = map_height
 	var noise = FastNoiseLite.new()
 	noise.seed = 12345
-	noise.noise_type = 0
+	noise.noise_type = 1
 	noise.fractal_octaves = 3
-	noise.frequency = 0.03
+	noise.frequency = 0.02
 	
 	var map: Array[Array]
 	for i in range(map_width):
@@ -26,15 +31,16 @@ func _ready():
 	
 	for i in range(map_width):
 		for j in range(3, map_height/1.5):
-			if noise.get_noise_2d(i, j) * weight(j, -10.0, map_height/1.5, 2) > 0.33:
+			if noise.get_noise_2d(i, j) * weight(j, 3.0, map_height/1.5, 150) > 0.33:
 				map[i][j] = 1
+	 
 	# Cleaning function
-	#for i in range(len(map)):
-		#for j in range(len(map[0])):
-			#if i > 0 and i < len(map)-1 and j > 0 and j < len(map[0])-1:
-				#if map[i][j] != 0:
-					#if map[i-1][j] == 0 and map[i+1][j] == 0 and map[i][j-1] == 0 and map[i][j+1] == 0:
-						#map[i][j] = 0
+	for i in range(len(map)):
+		for j in range(len(map[0])):
+			if i > 0 and i < len(map)-1 and j > 0 and j < len(map[0])-1:
+				if map[i][j] != 0:
+					if map[i-1][j] == 0 and map[i+1][j] == 0 and map[i][j-1] == 0 and map[i][j+1] == 0:
+						map[i][j] = 0
 	
 	# Main drawing cycle 
 	for i in range(map_width):

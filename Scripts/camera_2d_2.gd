fae528ef436070775238762676025421783b712e
d0nilo <email>
2024-11-11 14:58:41 +0200

    Implemented basic block placing/breaking

--- Scripts/camera_2d_2.gd
+++ Scripts/camera_2d_2.gd
@@ -1,27 +1,28 @@
 extends Camera2D
 
+var tile_pos = Vector2i(0, 0) #cursor position over the tilemap
+
 @onready var tile_map_layer = $"../../TileMapLayer"
 @onready var highlighter = $"../../TileMapLayer/Highlighter"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(tile_map_layer.seed)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#calculating cursor position and drawing tiles outline
 	var viewport_size = get_viewport().size / Vector2i(zoom)
 	var camera_top_left = Vector2i(get_screen_center_position()) - viewport_size / 2
 	var start_tile = tile_map_layer.local_to_map(camera_top_left)
 	var end_tile = tile_map_layer.local_to_map(camera_top_left + viewport_size - Vector2i(2, 2))
 	
 	var mouse_pos = tile_map_layer.get_local_mouse_position()
-	print(mouse_pos)
-	var tile_pos = mouse_pos.ceil() / Vector2(16.0, 16.0)
-	print(Vector2i(tile_pos))
-	print(start_tile)
-	print(end_tile)
+	tile_pos = mouse_pos.ceil() / Vector2(16.0, 16.0)
+	tile_pos = Vector2i(tile_pos.floor())
 	
 	highlighter.clear()
-	highlighter.set_cell(Vector2i(tile_pos.floor()), 0, Vector2i(0, 0), 0)
+	highlighter.set_cell(tile_pos, 0, Vector2i(0, 0), 0)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,25 @@
 extends CharacterBody2D
 
+@onready var tile_map_layer: TileMapLayer = $"../TileMapLayer"
+
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
+func block_place(tilepos):
+	#print("Block place")
+	tile_map_layer.set_cell(tilepos, 0, Vector2i(9, 3), 0)
+
+func block_break(tilepos):
+	#print("Block break")
+	#print(tile_map_layer.get_cell_tile_data(tilepos))
+	tile_map_layer.erase_cell(tilepos)
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed :
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			block_break($Camera2D2.tile_pos)
+		elif event.button_index == MOUSE_BUTTON_RIGHT and tile_map_layer.get_cell_tile_data($Camera2D2.tile_pos) == null:
+			block_place($Camera2D2.tile_pos)
 
 func _physics_process(delta):
 	# Add the gravity.
